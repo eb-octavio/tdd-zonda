30e6f10bb9490ad17f1c9bb566e6c5e166d83803
Octavio Augusto Coria <octavio@eventbrite.com>
2019-11-08 13:04:13 -0300

    Removing tests for implementation details.

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -4,10 +4,6 @@ from chess.board import Rook
 
 class TestBoard:
 
-    def test_it_should_be_instantiated(self):
-
-        board = Board()
-
     def test_it_can_settle_a_rook(self):
 
         board = Board()
@@ -16,11 +12,3 @@ class TestBoard:
         board.settle(rook, x='a', y='1')
 
         assert board.squares['a']['1'] == rook
-
-    def xtest_it_shold_move_the_rook_to_a_position(self):
-
-        board = Board()
-        rook = Rook()
-        board.settle(rook, x='a', y='1')
-
-        board.move(from_x, from_y, to_x, to_y)
